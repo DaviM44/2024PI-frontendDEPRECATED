c526fcb6050c99d4d3c21214a0da19b5294c9394
BR0KLYN9 <cauagran@gmail.com>
2024-05-15 16:51:02 -0300

    navbar

--- pi-frontend/src/app/app.module.ts
+++ pi-frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TelaLoginComponent } from './tela-login/tela-login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TelaLoginComponent
+    TelaLoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
